5c6ca4f63be14798eca2bf3a4fc3cc62cc97614f
Totny123 <24455370@qq.com>
2020-03-09 15:38:44 +0800

    添加了首页轮播图以及推荐功能

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :style="activeStyle"><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'TabBarItem',
+  props:{
+    path:String,
+    activeColor:{
+      type:String,
+      default:'red'
+    }
+  },
+  data(){
+      return {
+        // isActive:false
+      }
+  },
+  methods:{
+    itemClick(){
+      this.$router.push(this.path);
+    }
+  },
+  computed:{
+    isActive(){
+      // console.log(this.$route.path)
+      return this.$route.path.indexOf(this.path)!==-1
+    },
+    activeStyle(){
+      return this.isActive?{color:this.activeColor}:{}
+    }
+  }
+}
+</script>
+
+<style>
+.tab-bar-item{
+  flex: 1;
+  text-align: center;
+  height:49px;
+  box-shadow: 0 -1px 1px rgba(100, 100, 100, .08)
+}
+
+.tab-bar-item img{
+  height:24px;
+  width:24px;
+  vertical-align:top;
+  margin-top: 3px;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,55 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+const Home = ()=>import('views/home/Home')
+const Category = ()=>import('views/category/Category')
+const Cart = ()=>import('views/cart/Cart')
+const Profile = ()=>import('views/profile/Profile')
+
+Vue.use(Router)
+
+const routes=[
+  {
+    path:'',
+    redirect:'/home'
+  },
+  {
+    path:'/home',
+    component:Home,
+    meta:{
+      title:'首页'
+    }
+  },
+  {
+    path:'/category',
+    component:Category,
+    meta:{
+      title:'分类'
+    }
+  },
+  {
+    path:'/cart',
+    component:Cart,
+    meta:{
+      title:'购物车'
+    }
+  },
+  {
+    path:'/profile',
+    component:Profile,
+    meta:{
+      title:'我的'
+    }
+  }
+]
+
+const router =new Router({
+  routes,
+  mode:'history'
+})
+
+router.beforeEach((to,from,next)=>{
+  document.title = to.matched[0].meta.title
+  next()
+})
+
+export default router
